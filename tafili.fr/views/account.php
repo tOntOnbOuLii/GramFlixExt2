c7e42a9385aaa5bfc7df116ac86c516dee02ffca
tOntOnbOuLii <tontonboulii43@gmail.com>
2025-11-16 19:37:57 +0100

    Delete tafili.fr directory

--- tafili.fr/views/account.php
+++
@@ -1,5 +0,0 @@
-<section class="card">
-    <h2>Mon compte</h2>
-    <p>Mettez a jour votre mot de passe regulierement. Les modifications sont immediates.</p>
-    <div id="account-app"></div>
-</section>
--- tafili.fr/views/providers.php
+++
@@ -1,5 +0,0 @@
-<section class="card">
-    <h2>Providers</h2>
-    <p>Ajoutez ou modifiez les sites disponibles dans vos extensions GramFlix. Les modifications sont enregistrees dans <code>data/providers.json</code>.</p>
-    <div id="providers-app"></div>
-</section>
