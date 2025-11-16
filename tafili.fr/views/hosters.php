41ba5f36c053afc47df39965c587640a80c6f4ba
tOntOnbOuLii <tontonboulii43@gmail.com>
2025-11-16 19:37:57 +0100

    Delete tafili.fr directory

--- tafili.fr/views/hosters.php
+++
@@ -1,5 +0,0 @@
-<section class="card">
-    <h2>Hosters</h2>
-    <p>Maintenez la liste des miroirs et hebergeurs supportes par les extensions (fichier <code>data/hosters.json</code>).</p>
-    <div id="hosters-app"></div>
-</section>
